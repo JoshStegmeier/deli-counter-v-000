9c478e80020eeddb9b5254e89c1a40d3e7eb60ad
Josh Stegmeier <josh@steg.me>
2018-05-18 18:53:01 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,3 +18,13 @@ def take_a_number(katz_deli, name)
   place_in_line = katz_deli.index(name) + 1
   puts "Welcome, #{name}. You are number #{place_in_line} in line."
 end
+
+def now_serving(katz_deli)
+  line_size = katz_deli.size
+  if line_size > 0
+    next_up = katz_deli.shift
+    puts "Currently serving #{next_up}."
+  else
+    puts "There is nobody waiting to be served!"
+  end
+end
