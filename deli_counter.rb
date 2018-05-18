eb3af64aefb2009f202baf25d36fae2443db93e2
Josh Stegmeier <josh@steg.me>
2018-05-18 18:41:03 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,5 +4,10 @@ def line(katz_deli)
   line_size = katz_deli.size
   if line_size == 0
     puts "The line is currently empty."
+  else
+    line_str = "The line is currently:"
+    katz_deli.each_with_index do | name, index |
+      line_str + " #{index}. #{name}"
+    end
   end
 end
